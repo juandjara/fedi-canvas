1a5b82e3486e4e35f7f74f74feac0a86fef108f1
Juan D. Jara <juanorigami@gmail.com>
2023-09-13 16:37:19 +0200

    finished jwt verification

--- app/routes/api/pixel.ts
+++ app/routes/api/pixel.ts
@@ -1,7 +1,13 @@
+import { WAIT_TIME } from "@/lib/constants"
 import { withRedis } from "@/lib/db.server"
 import getColorIndex from "@/lib/utils/getColorIndex"
 import hexToRgb from "@/lib/utils/hexToRgb"
 import type { ActionFunction } from "@remix-run/node"
+import { jwtVerify } from "jose"
+
+const secretEnv = process.env.JWT_SECRET
+const JWT_SECRET = secretEnv ? new TextEncoder().encode(secretEnv) : null
+const BOT_AGENT = 'FediCanvas bot v0.0.1'
 
 export const action: ActionFunction = async ({ request }) => {
   const { x, y, color } = await request.json()
@@ -15,16 +21,49 @@ export const action: ActionFunction = async ({ request }) => {
     return new Response('Invalid input. color in json body should be a string', { status: 400 })
   }
 
-  // TODO: validate user
+  if (!JWT_SECRET) {
+    return new Response('JWT_SECRET is not set', { status: 500 })
+  }
 
-  return withRedis(async redis => {
-    const index = getColorIndex(x, y)
-    const [r, g, b] = hexToRgb(color)
-    return redis.pipeline()
-      .bitfield('canvas', 'SET', 'u8', `#${index + 0}`, r)
-      .bitfield('canvas', 'SET', 'u8', `#${index + 1}`, g)
-      .bitfield('canvas', 'SET', 'u8', `#${index + 2}`, b)
-      .bitfield('canvas', 'SET', 'u8', `#${index + 3}`, 255)
-      .exec()
-  })
+  const jwt = (request.headers.get('Authorization') || '').split(' ')[1]
+  if (!jwt) {
+    return new Response('Authorization header is missing', { status: 401 })
+  }
+
+  try {
+    const { payload } = await jwtVerify(jwt, JWT_SECRET, {
+      algorithms: ['HS256'],
+      issuer: BOT_AGENT
+    })
+    const user = payload.sub
+    return withRedis(async redis => {
+      const lastTime = await redis.get(`time:${user}`)
+      if (lastTime) {
+        const now = Date.now()
+        const diff = now - Number(lastTime)
+        if (diff < WAIT_TIME) {
+          return new Response(
+            JSON.stringify({
+              message: 'Too many requests',
+              lastTime,
+            }),
+            { status: 429 }
+          )
+        }
+      }
+
+      await redis.setex(`time:${user}`, WAIT_TIME / 1000, Date.now())
+
+      const index = getColorIndex(x, y)
+      const [r, g, b] = hexToRgb(color)
+      return redis.pipeline()
+        .bitfield('canvas', 'SET', 'u8', `#${index + 0}`, r)
+        .bitfield('canvas', 'SET', 'u8', `#${index + 1}`, g)
+        .bitfield('canvas', 'SET', 'u8', `#${index + 2}`, b)
+        .bitfield('canvas', 'SET', 'u8', `#${index + 3}`, 255)
+        .exec()
+    })
+  } catch (err) {
+    return new Response('Invalid JWT', { status: 401 })
+  }
 }
