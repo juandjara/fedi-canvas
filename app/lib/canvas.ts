dd31bc9ebbad666d5bc174ec352961615d6f59d9
Juan D. Jara <juanorigami@gmail.com>
2023-09-14 10:58:09 +0200

    fix initial buffer setting

--- app/lib/canvas.ts
+++ app/lib/canvas.ts
@@ -2,12 +2,17 @@ import { TEXTURE_LENGTH } from "./constants"
 import { withRedis } from "./db.server"
 
 export async function getCanvas() {
-  const resBuffer = Buffer.alloc(TEXTURE_LENGTH, 0xff)
-  const dbBuffer = await withRedis(async redis => redis.getBuffer('canvas'))
-  if (dbBuffer) {
-    resBuffer.set(dbBuffer)
-  }
-  return resBuffer
+  const dbBuffer = await withRedis(async redis => {
+    const buffer = await redis.getBuffer('canvas')
+    if (buffer) {
+      return buffer
+    } else {
+      const whiteEmptyBuffer = Buffer.alloc(TEXTURE_LENGTH, 0xff)
+      await redis.set('canvas', whiteEmptyBuffer)
+      return whiteEmptyBuffer
+    }
+  })
+  return dbBuffer
 }
 
 export async function savePixel(x: number, y: number, color: string) {
